3ae41cd24f3339bededefc475ad81e8159a6872b
Jiwon Baek <wldnjsl2001@naver.com>
2023-11-10 22:03:58 +0900

    디자인 수정, 사용자별 다른 색 사용하여 이름 표시

--- chat.js
+++ chat.js
@@ -1,34 +1,51 @@
 
 const dummyData = [
-    { user: "Alice", message: "Hello there!" },
-    { user: "Bob", message: "Hi Alice!" },
-    { user: "Alice", message: "How are you doing?" },
-    // 추가 데이터를 필요에 따라 여기에 계속 추가할 수 있습니다.
-  ];
+  { user: "Alice", message: "Hello there!" },
+  { user: "Bob", message: "Hi Alice!" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" },
+  { user: "Alice", message: "How are you doing?" }
+];
 
-  // 채팅을 화면에 추가하는 함수
-  function appendMessage(user, message) {
-    const chatMessages = document.getElementById("chat-messages");
-    const messageDiv = document.createElement("div");
-    messageDiv.classList.add("message");
-    messageDiv.innerHTML = `<span class="user">${user}:</span> ${message}`;
-    chatMessages.appendChild(messageDiv);
-    chatMessages.scrollTop = chatMessages.scrollHeight; // 스크롤을 항상 아래로 유지
-  }
+const time = [2000,2000,2000,2000,2000,2200,2000,2000,2000,5000,5000,5000,5,5,5,3,5,5,5,5,5,5,5]
+const color = ['blue', 'green', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'orange']
 
-  // 더미 데이터를 2초마다 하나씩 표시
-  function displayDummyData() {
-    let index = 0;
-    const intervalId = setInterval(() => {
-      if (index < dummyData.length) {
-        const { user, message } = dummyData[index];
-        appendMessage(user, message);
-        index++;
-      } else {
-        clearInterval(intervalId);
-      }
-    }, 2000);
-  }
+// 메세지 출력
+function appendMessage(user, message, color) {
+  const chatMessages = document.getElementById("chat-messages");
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+  messageDiv.innerHTML = `<span class="user" style="color: ${color};">${user}:</span> ${message}`;
+  chatMessages.appendChild(messageDiv);
+  chatMessages.scrollTop = chatMessages.scrollHeight; // 스크롤을 항상 아래로 유지
+}
 
-  // 초기 실행
-  displayDummyData();
+// 더미데이타 정보 보내기
+function displayDummyData() {
+  let index = 0;
+  const intervalId = setInterval(() => {
+    if (index < dummyData.length) {
+      const { user, message } = dummyData[index];
+      const userColor = color[index];
+      appendMessage(user, message, userColor);
+      index++;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, time[index]);
+}
+
+// 초기 실행
+displayDummyData();
